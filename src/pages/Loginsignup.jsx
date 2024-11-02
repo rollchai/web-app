7b1346e70c7ffe5c33831f1069837664404468df
Akshit <168993962+rollchai@users.noreply.github.com>
2024-11-02 23:35:46 +0530

    final work

--- src/pages/Loginsignup.jsx
+++ src/pages/Loginsignup.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/Loginsignup.css"
 const Loginsignup = () => {
+  const [state,setstate]=useState("Login")
+  const [formData,setformData]=useState({
+    username:"",
+    password:"",
+    email:"",
+
+  })
+  const changehandler=(e)=>{
+setformData({...formData,[e.target.name]:e.target.value})
+  }
+  const login=async()=>{
+  console.log("login function Executed",formData)
+  }
+  const signup =()=>{
+    console.log("signup function Executed",formData)
+  }
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Username"/>
-          <input type="email" placeholder="Email"/>
-          <input type='password ' placeholder='password'/>
+          {state==="sign up"?<input type="text" placeholder="Username" name='username' value={formData.username} onChange={
+            changehandler
+          }/>:<></>}
+          <input name='email' value={formData.email} onChange={changehandler} type="email" placeholder="Email"/>
+          <input  name="password" value={formData.password} onChange={
+            changehandler
+          } type='password ' placeholder='password'/>
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Aleready have an account? <span>login Here</span></p>
+        <button onClick={()=>{
+          state==="Login"?login():signup()
+        }}>Continue</button>
+        {state==="sign up"? <p className="loginsignup-login">Already have an account? <span onClick={()=>{
+          setstate("Login")
+        }}>login Here</span></p>:<p className="loginsignup-login">Create an account? <span onClick={()=>{
+          setstate("sign up")
+        }}>click Here</span></p>}
+       
+        
         <div className="loginsignup-agree">
           <input type='checkbox' name="" id=""/>
-          <p>By continuing i afree to the terms of use & privacy policies</p>
+          <p>By contineuing i agree to the terms of use & privacy policies</p>
         </div>
       </div>
 
